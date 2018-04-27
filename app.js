ce1af1b5e78020f95349d95d29b32d7333244d80
elguyen <baotrung.nguyen.10f@gmail.com>
2018-04-27 14:56:22 +0200

    add action: ping,listen

--- app.js
+++ app.js
@@ -1,13 +1,56 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+var rp = require('request-promise');
+const app = express()
 
-$port = parseInt(process.argv[2],10);
-$host = process.argv[3];
+const action = process.argv[2]
+const serverArr = process.argv[3].split(':')
+const host = serverArr[0]
+const port = serverArr[1]
 
-app.get('/ping', function (req, res) {
-    res.send('Hello World!')
-})
+const listen = (_host,_port) => {
+    app.get('/ping', function (req, res) {
+        res.send('ok')
+    })
+    
+    app.listen(_port,_host, function () {
+        console.log('Example app listening on port ' + _port + "!")
+    })
+}
 
-app.listen($port,$host, function () {
-    console.log('Example app listening on port ' + $port + "!");
-  })
+const ping = (_host,_port) => {
+    const url = 'http://'+_host+':'+_port+'/ping'
+    console.log('pinging ' + url)
+    var options = {
+        uri: url,
+        headers: {
+            'User-Agent': 'Request-Promise'
+        },
+        json: true
+    };
+    
+    rp(options)
+        .then(function (res) {
+            console.log(res)
+        })
+        .catch(function (err) {
+            console.log('haha',err.message)
+        });
+}
+
+
+
+switch (action){
+    case 'listen': 
+        listen(host,port) 
+        break
+    case 'ping':
+        ping(host,port)
+        break
+    default :
+        console.log('action unknow! (' + action + ')')
+        break
+
+}
+
+
+
